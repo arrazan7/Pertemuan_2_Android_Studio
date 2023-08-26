8cae906afdf7de3049ce13f5a46047c348278720
Fayyadh Arrazan Miftakhul <113814875+arrazan7@users.noreply.github.com>
2023-08-26 21:40:17 +0700

    Commit ketiga

--- app/src/main/java/com/example/pertemuan_2/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan_2/MainActivity.kt
@@ -8,7 +8,7 @@ import com.example.pertemuan_2.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var number = 0
-//    ini hanya komentar
+//    ini hanya komentar saja
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
